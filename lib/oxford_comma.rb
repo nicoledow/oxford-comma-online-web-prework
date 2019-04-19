fe74d09c353a1483e80fb77f7f439e9e15f52597
Nicole Dow <nikki.dow71@gmail.com>
2019-04-19 19:49:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
+  new_str = ""
+ 
+ if array.size == 2
+   new_str << array[0].to_s + "and" + array[1].to_s
+   
+array.each_with_index do |ele, i|
+  if i < array.size - 2
+    new_str << ele.to_s + ", "
+  elsif i == array.size - 2
+    new_str << ele.to_s + ", and "
+  else
+    new_str << ele.to_s
+  end
+end
+  
+  return new_str
 
 end
