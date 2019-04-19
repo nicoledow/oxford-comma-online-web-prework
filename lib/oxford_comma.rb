4bbb006ba84f2fbbdf9791b9be40a3a997fda60f
Nicole Dow <nikki.dow71@gmail.com>
2019-04-19 19:46:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,16 @@
 def oxford_comma(array)
   new_str = ""
+ 
+array.each_with_index do |ele, i|
+  if i < array.size - 2
+    new_str << ele.to_s + ", "
+  elsif i == array.size - 2
+    new_str << ele.to_s + ", and "
+  else
+    new_str << ele.to_s
+  end
+end
   
-  if array.size == 1
-    return array.to_s
+  return new_str
 
 end
