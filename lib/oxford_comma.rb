0862a0737c017c0ec52a90625099df44b8cf013d
Nicole Dow <nikki.dow71@gmail.com>
2019-04-19 19:49:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   new_str = ""
  
+ if array.size == 2
+   new_str << array[0].to_s + "and" + array[1].to_s
+   
 array.each_with_index do |ele, i|
   if i < array.size - 2
     new_str << ele.to_s + ", "
